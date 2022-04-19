fa7d51cf45d6666d5b7c2078086dacb471d8aa61
MattM <67703490+Matthew-Muir@users.noreply.github.com>
2022-04-19 14:08:26 -0700

    Completed TicTacToe Game

--- BlazorServerWebsite/Pages/TicTacToe/GameState.cs
+++ BlazorServerWebsite/Pages/TicTacToe/GameState.cs
@@ -3,6 +3,35 @@
     public class GameState
     {
         public bool IsXTurn { get; set; } = true;
+        public Dictionary<int, Square> Squares;
 
+        public GameState()
+        {
+            Squares = new Dictionary<int, Square>();
+            for (int i = 0; i < 9; i++)
+            {
+                Square square = new Square();
+                Squares.Add(i, square);
+            }
+        }
+
+        public int CheckWin()
+        {
+            for (int i = 0; i < winningCombos.GetLength(0) - 1; i++)
+            {
+
+                var s1 = Squares.GetValueOrDefault(winningCombos[i,0]);
+                var s2 = Squares.GetValueOrDefault(winningCombos[i,1]);
+                var s3 = Squares.GetValueOrDefault(winningCombos[i,2]);
+                if(s1?.Icon == s2?.Icon && s1?.Icon == s3?.Icon)
+                {
+                    return s1.Icon;
+                }
+
+            }
+            return -1;
+        }
+
+        int[,] winningCombos = new int[,] { { 0, 1, 2 }, { 0, 4, 8 }, { 0, 3, 6 }, { 1, 4, 7 }, { 2, 5, 8 }, { 2, 4, 6 }, { 3, 4, 5 }, { 6, 7, 8 } };
     }
 }
---
+++ BlazorServerWebsite/Pages/TicTacToe/Square.cs
@@ -0,0 +1,31 @@
+﻿namespace BlazorServerWebsite.Pages.TicTacToe
+{
+    public class Square
+    {
+        public int Position { get; set; }
+        public string[] Classes { get; set; } = default!;
+        public string IconClass
+        {
+            get
+            {
+                switch (Icon)
+                {
+                    case 0:
+                        return "oi oi-target";
+                    case 1:
+                        return "oi oi-x";
+                    default:
+                        return "";
+
+                }
+            }
+            set { }
+        }
+        public int Icon { get; set; } = -1;
+
+        public string StylesToString()
+        {
+            return String.Join(' ', Classes);
+        }
+    }
+}
