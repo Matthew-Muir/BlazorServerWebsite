e59b59c79e36bb0f36c2d39c401ae3ad5d4628ec
MattM <67703490+Matthew-Muir@users.noreply.github.com>
2022-04-25 20:59:43 -0700

    Read file text completed.

--- BlazorServerWebsite/Pages/FileReading/FileReading.razor.cs
+++ BlazorServerWebsite/Pages/FileReading/FileReading.razor.cs
@@ -19,6 +19,25 @@
     {
         public string Txt { get; set; } = default!;
 
-        public async Task ReadText(OnCh)
+        public async Task ReadText(InputFileChangeEventArgs e)
+        {
+            var file = e.File;
+            byte[] result;
+            using (var stream = file.OpenReadStream())
+            {
+                result = new byte[stream.Length];
+                await stream.ReadAsync(result, 0, (int)stream.Length);
+            }
+            Txt = Encoding.UTF8.GetString(result);
+        }
+
+        public async Task SaveFile(InputFileChangeEventArgs e)
+        {
+            var path = @"C:\Users\coded\Documents\GitHub\BlazorServerWebsite\BlazorServerWebsite\Uploads\";
+            var file = e.File;
+            await using FileStream fs = new(path, FileMode.Create);
+            await file.OpenReadStream().CopyToAsync(fs);
+        }
+
     }
 }
