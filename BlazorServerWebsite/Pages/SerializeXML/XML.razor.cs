e369a23158bf2d5232e899d48e7c5665d95f08a0
MatthewMuir-MLI <106112115+MatthewMuir-MLI@users.noreply.github.com>
2022-06-27 16:59:47 -0700

    WIP XML serialization

---
+++ BlazorServerWebsite/Pages/SerializeXML/XML.razor.cs
@@ -0,0 +1,40 @@
+﻿namespace BlazorServerWebsite.Pages.SerializeXML
+{
+    public partial class XML
+    {
+
+        [Inject]
+        public ILogger<XML>? Logger { get; set; }
+
+        public class Book
+        {
+            public String title;
+        }
+
+        public void WriteXML()
+        {
+            var overview = new List<Book>();
+            overview.Add(new Book() { title = "dance" });
+            overview.Add(new Book() { title = "sing" });
+            System.Xml.Serialization.XmlSerializer writer = new System.Xml.Serialization.XmlSerializer(typeof(List<Book>));
+
+            var path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "//SerializationOverview.xml";
+            System.IO.FileStream file = System.IO.File.Create(path);
+
+            writer.Serialize(file, overview);
+            file.Close();
+        }
+
+        public void CheckFile()
+        {
+            var file = new FileInfo(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "//SerializationOverview.xml");
+            Logger.LogWarning(file.FullName);
+            Logger.LogWarning(file.CreationTime.ToString());
+        }
+
+    }
+
+   
+
+}
+
